58567f3b78d033f512329141617d33c466e1adb8
michaelneu <git@michaeln.eu>
2018-04-21 16:44:54 +0200

    Add project files.

---
+++ P2P.Enroll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P2P.Enroll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
